79686f6adcc44c05abbd8148b91c6e732307d7a9
Maxime Augier <max@xolus.net>
2013-10-17 23:03:04 +0200

    Fix transform, wrap in class

--- transform.py
+++ transform.py
@@ -1,12 +1,16 @@
 
-all_transforms = {"Evil Twins": evil_twins,
-                  "L33ts": leet}
+class Transform:
+	def __init__(self,name,call):
+		self.name = name
+		self.call = call
+	def __call__(self,*k,**kw):
+		return self.call(*k, **kw)
 
 def replace_many(words):
 	maxlen = max(len(x) for x in words)
 	def fun(s):
 		while len(s):
-			for l in range(maxlen,1,-1):
+			for l in range(maxlen,0,-1):
 				if s[:l] in words:
 					yield words[s[:l]]
 					s = s[l:]
@@ -27,15 +31,20 @@ evil_twins = replace_many({
 	":<":":>",
 	":')":":'(",
 	":]":":[",
-	":D","D:",
-	":x",":o",
-	":P",":6",
-	":p",":6"})
+	":D":"D:",
+	":x":":o",
+	":P":":6",
+	":p":":6"})
 
 leet = replace_many({
 	"e":"3",
 	"a":"4",
 	"s":"5",
-	"i","1",
-	"o","0",
-	"b","8"})
+	"i":"1",
+	"o":"0",
+	"b":"8"})
+
+
+all_transforms = [Transform("maléfiques", evil_twins),
+                  Transform("l33ts", leet),
+		  Transform("bruyants", lambda s: s.upper())]
