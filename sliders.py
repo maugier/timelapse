a4b9ea68b24ae8b068466a530678e52d10518b87
Maxime Augier <max@xolus.net>
2013-10-17 22:52:07 +0200

    Stream merge

--- sliders.py
+++ sliders.py
@@ -1,32 +1,38 @@
 import datetime
 import random
 import transform
-import timelapse
+#import timelapse
 
 def merge(s1, s2):
+    try:
+        x1 = next(s1)
+    except StopIteration:
+        yield from s2
+        return
+
+    try:
+        x2 = next(s2)
+    except StopIteration:
+        yield from s1
+        return
+
     while True:
-        try:
-            x1 = next(s1)
-        except StopIteration:
-            yield from s2
-
-        try:
-            x2 = next(s2)
-        except StopIteration:
-            yield from s1
-
         if x2 > x1:
             yield x1
             try:
                 x1 = next(s1)
             except StopIteration:
+                yield x2
                 yield from s2
+                return
         else:
             yield x2
             try:
                 x2 = next(s2)
             except StopIteration:
+                yield x1
                 yield from s1
+                return
         
 
 def sliding_stream(delay=20):
@@ -34,5 +40,5 @@ def sliding_stream(delay=20):
     while True:
         yield(now, random.choice(transform.all_transforms))
 
-class Sliders(timelapse.TimeLapse):
-    
+#class Sliders(timelapse.TimeLapse):
+#   pass 
