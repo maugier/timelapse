01192fe3454ee3b1dd2eebfe830c4d984bb52002
Maxime Augier <max@xolus.net>
2013-10-17 22:04:56 +0200

    Begin work on random transforms

---
+++ transform.py
@@ -0,0 +1,31 @@
+
+def replace_many(words):
+	maxlen = max(len(x) for x in words)
+	def fun(s):
+		while len(s):
+			for l in range(maxlen,1,-1):
+				if s[:l] in words:
+					yield words[s[:l]]
+					s = s[l:]
+					break
+			else:
+				yield s[:1]
+				s = s[1:]
+	def fun2(s):
+		return "".join(fun(s))
+	return fun2
+
+evil_twins = replace_many({
+	":)":":(",
+	":(":":)",
+	":))":":((",
+	":')":":'(",
+	":]":":[",
+	":D","D:",
+	":x",":o",
+	":P",":6",
+	":p",":6"})
+
+leet = replace_many({
+	"e":"3",
+	"a":"4"})
